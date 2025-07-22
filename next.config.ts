d3efd55c51d1e423ddf6b9bb8cca6920f76c51bc
TeamPapaya <34426841+edmundackah@users.noreply.github.com>
2025-07-22 20:17:05 +0100

    please work

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,20 @@
-import type {NextConfig} from "next";
+import path from "path";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // basePath: '/devops/aws/migration',
-  output: 'export',
+  output: "export",
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.icons8.com',
-        pathname: '/win10/512/**',
+        protocol: "https",
+        hostname: "img.icons8.com",
+        pathname: "/win10/512/**",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@/"] = path.resolve(__dirname, "src/");
+    return config;
   },
 };
 
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Header } from "../../components/header";
+import { Header } from "@/components/header";
 import { ScrollToTop } from "@/components/scroll-to-top";
 import { Separator } from "@/components/ui/separator";
 import { Footer } from "@/components/footer";
